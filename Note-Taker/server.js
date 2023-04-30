cc4eba97ebfbe828b956c245748c6f1c55a23e50
Anthony Vaquer <anthonymark126@gmail.com>
2023-04-30 10:35:59 -0700

    server.js

---
+++ Note-Taker/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const PORT = process.env.PORT || 3001;
+
+
+// middleware
+// questions about top 3
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/frontEndRoutes', frontEndRoutes);
+app.use('/backEndRoutes', backEndRoutes);
