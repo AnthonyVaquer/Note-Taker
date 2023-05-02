1cac197ecb40096aa7faf652182ce5f1a64720d3
Anthony Vaquer <anthonymark126@gmail.com>
2023-05-02 15:07:23 -0700

    backEndRoute

--- Note-Taker/routes/backEndRoutes.js
+++ Note-Taker/routes/backEndRoutes.js
@@ -1,4 +1,15 @@
 const router = require('express').Router();
+const { readFromFile, readAndAppend } = require('../db/fsUtils.js');
+// /api/notes  GET ROUTE need to retun all the notes onto the html
+notes.get("/notes", (req, res)=> {
+    console.info(`${req.method} request for notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 
+})
+
+// POST route to /api/notes needs to allow the user to add a new note
+notes.post("/notes", (req, res)=> {
+    
+})
 
 module.exports = router
